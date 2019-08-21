61d80c1a5b008db337e34704531af3cc8bb4c7fb
Ahmad Guenoun <amguenoun@gmail.com>
2019-08-21 15:27:47 -0400

    imported teamData and set up useState hook

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
+import teamData from "./teamData"
+
 function App() {
+  const [teamList, setTeamList] = useState(teamData);
+  console.log(teamList);
   return (
     <div className="App">
       <header className="App-header">
