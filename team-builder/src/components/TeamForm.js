a3fb0a4802737729c2c648cda5bedb7d67acfd06
Ahmad Guenoun <amguenoun@gmail.com>
2019-08-21 16:10:14 -0400

    added state property that updates when input fields change

--- team-builder/src/components/TeamForm.js
+++ team-builder/src/components/TeamForm.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const TeamForm = (props) => {
+    const [teamMember, setTeamMember] = useState({ name: "", email: "", role: "" });
+
+    const changeTeamMember = (event) => {
+        setTeamMember({ ...teamMember, [event.target.name]: event.target.value });
+    };
+    console.log(teamMember);
     return (
         <div>
             <form>
                 <label htmlFor="name">Name: </label>
-                <input type="text" placeholder="Name" name="name" />
+                <input type="text" placeholder="Name" name="name" onChange={e => changeTeamMember(e)} />
                 <label htmlFor="email">Email: </label>
-                <input type="text" placeholder="Email" name="email" />
+                <input type="email" placeholder="Email" name="email" onChange={e => changeTeamMember(e)} />
                 <label htmlFor="role">Role: </label>
-                <input type="text" placeholder="Role" name="role" />
+                <input type="text" placeholder="Role" name="role" onChange={e => changeTeamMember(e)} />
                 <button type="submit">Add Team Member!</button>
             </form>
         </div>
